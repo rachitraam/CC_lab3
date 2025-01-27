57d188900de95dae7c370798c5c966fa74541518
rachitraam <rachit.guruprasad@gmail.com>
2025-01-27 21:26:33 +0530

    Lab3

--- test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-try:
-    conn = sqlite3.connect("auth.db")
-    print("SQLite connected successfully")
-    conn.close()
-except sqlite3.Error as e:
-    print(f"SQLite error: {e}")
-
